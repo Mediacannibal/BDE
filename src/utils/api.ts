d6a0f7e1a09057cecff035632f3d74e84fbd617c
kiranraj <kiranraj4939@gmail.com>
2021-03-18 19:28:41 +0530

    Api services

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,5 +1,7 @@
 import instance from 'utils/axios'
 
+// User
+
 export const Sociallogin = (callback: (arg0: any, arg1: string) => void, data: any) => {
   instance.post(`/user/social/signup/`, data, {
     headers: {
@@ -9,8 +11,27 @@ export const Sociallogin = (callback: (arg0: any, arg1: string) => void, data: a
     .catch(err => callback(err, err.response))
 }
 
-export const signup = (callback: (arg0: any, arg1: string) => void, data: any) => {
-  instance.post(`/user/social/signup/normal/`, data, {
+export const profile = (callback: (arg0: any, arg1: string) => void) => {
+  instance.get(`/user/auth/profile/`, {
+    headers: {
+      'Authorization': 'Token'
+    }
+  }).then((res) => { callback(res, 'sucess') })
+    .catch(err => callback(err, err.response))
+}
+
+export const editProfileImage = (callback: (arg0: any, arg1: string) => void, token: any, data: any, id: any) => {
+  instance.put(`/user/edit_profile_image/` + id + `/`, data, {
+    headers: {
+      'Authorization': token ? `Token ${token}` : '',
+      'Content-Type': 'application/json'
+    }
+  }).then((res) => { callback(res, 'sucess') })
+    .catch(err => callback(err, err.response))
+}
+
+export const otpVerify = (callback: (arg0: any, arg1: string) => void, data: any) => {
+  instance.post(`/user/phone/email/otp_verify/`, data, {
     headers: {
       "Content-Type": "application/json",
     }
@@ -18,27 +39,8 @@ export const signup = (callback: (arg0: any, arg1: string) => void, data: any) =
     .catch(err => callback(err, err.response))
 }
 
-export const createaccount = (callback: (arg0: any, arg1: string) => void, data: any, token: any) => {
-  instance.post(`/user/social/signup/admin/`, data, {
-    headers: {       'Authorization': token ? `Token ${token}` : '',
-      'Content-Type': 'application/json'
-    }
-  }).then((res) => { callback(res, 'sucess') })
-    .catch(err => callback(err, err.response))
-}
-
-
-export const updateProfile = (callback: (arg0: any, arg1: string) => void, token: any, data: any,id:any) => {
-  instance.put(`/user/edit_profile/`+id+`/`, data, {
-    headers: {       'Authorization': token ? `Token ${token}` : '',
-      'Content-Type': 'application/json'
-    }
-  }).then((res) => { callback(res, 'sucess') })
-    .catch(err => callback(err, err.response))
-}
-
-export const postAppdata = (callback: (arg0: any, arg1: string) => void, token: any, data: any) => {
-  instance.post(`/appdata/bidrecordlist/?start_date=`, data, {
+export const editProfile = (callback: (arg0: any, arg1: string) => void, token: any, data: any, id: any) => {
+  instance.put(`/user/edit_profile/` + id + `/`, data, {
     headers: {
       'Authorization': token ? `Token ${token}` : '',
       'Content-Type': 'application/json'
@@ -47,18 +49,46 @@ export const postAppdata = (callback: (arg0: any, arg1: string) => void, token: 
     .catch(err => callback(err, err.response))
 }
 
-
-export const getResultAppdata = (callback: (arg0: any, arg1: string) => void) => {
-  instance.get(`/appdata/gamedata/result/`, {
+export const forgotPassword = (callback: (arg0: any, arg1: string) => void, data: any) => {
+  instance.post(`/user/forgot_password/`, data, {
     headers: {
-      'Content-Type': 'application/json'
+      "username_email_or_phone": "adiakash3@gmail.com",
     }
   }).then((res) => { callback(res, 'sucess') })
     .catch(err => callback(err, err.response))
 }
 
-export const getResultpresentAppdata = (callback: (arg0: any, arg1: string) => void, token: any) => {
-  instance.get(`/appdata/gamedatalist/present/`, {
+export const logout = (callback: (arg0: any, arg1: string) => void, data: any) => {
+  instance.post(`/user/logout/`, data, {
+    headers: {
+      "Content-Type": "application/json",
+    }
+  }).then((res) => { callback(res, 'sucess') })
+    .catch(err => callback(err, err.response))
+}
+
+export const logoutall = (callback: (arg0: any, arg1: string) => void, data: any) => {
+  instance.post(`/user/logoutall/`, data, {
+    headers: {
+      "Authorization": "Token",
+    }
+  }).then((res) => { callback(res, 'sucess') })
+    .catch(err => callback(err, err.response))
+}
+
+// Task
+
+export const addProject = (callback: (arg0: any, arg1: string) => void) => {
+  instance.get(`/task/project/add/`, {
+    headers: {
+      'Authorization': 'Token'
+    }
+  }).then((res) => { callback(res, 'sucess') })
+    .catch(err => callback(err, err.response))
+}
+
+export const editProject = (callback: (arg0: any, arg1: string) => void, token: any, data: any, id: any) => {
+  instance.put(`/task/project/edit/` + id + `/`, data, {
     headers: {
       'Authorization': token ? `Token ${token}` : '',
       'Content-Type': 'application/json'
@@ -67,8 +97,8 @@ export const getResultpresentAppdata = (callback: (arg0: any, arg1: string) => v
     .catch(err => callback(err, err.response))
 }
 
-export const getResultbydateAppdata = (callback: (arg0: any, arg1: string) => void, token: any, date: any) => {
-  instance.get(`/appdata/gamedatalist/present/?start_date=` + date, {
+export const editTask = (callback: (arg0: any, arg1: string) => void, token: any, data: any, id: any) => {
+  instance.put(`/task/task/edit/` + id + `/`, data, {
     headers: {
       'Authorization': token ? `Token ${token}` : '',
       'Content-Type': 'application/json'
@@ -77,8 +107,26 @@ export const getResultbydateAppdata = (callback: (arg0: any, arg1: string) => vo
     .catch(err => callback(err, err.response))
 }
 
-export const getResultfutureAppdata = (callback: (arg0: any, arg1: string) => void, token: any) => {
-  instance.get(`/appdata/gamedatalist/future/`, {
+export const taskList = (callback: (arg0: any, arg1: string) => void) => {
+  instance.get(`/task/task/list/`, {
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  }).then((res) => { callback(res, 'sucess') })
+    .catch(err => callback(err, err.response))
+}
+
+export const projectAdd = (callback: (arg0: any, arg1: string) => void) => {
+  instance.post(`/task/project/add/`, {
+    headers: {
+      'Authorization': 'Token'
+    }
+  }).then((res) => { callback(res, 'sucess') })
+    .catch(err => callback(err, err.response))
+}
+
+export const dashboardCreate = (callback: (arg0: any, arg1: string) => void, token: any) => {
+  instance.get(`/task/dashboad_create/`, {
     headers: {
       'Authorization': token ? `Token ${token}` : '',
       'Content-Type': 'application/json'
@@ -87,83 +135,10 @@ export const getResultfutureAppdata = (callback: (arg0: any, arg1: string) => vo
     .catch(err => callback(err, err.response))
 }
 
-export const getResultTodaydata = (callback: (arg0: any, arg1: string) => void) => {
-  instance.get(`/appdata/gamedata/currentday/`, {
+export const createDashboard = (callback: (arg0: any, arg1: string) => void) => {
+  instance.post(`/task/dashboad_create/`, {
     headers: {
-      'Content-Type': 'application/json'
-    }
-  }).then((res) => { callback(res, 'sucess') })
-    .catch(err => callback(err, err.response))
-}
-
-// &number=2",
-export const getBidlogdata = (callback: (arg0: any, arg1: string) => void, token: any) => {
-  instance.get(`/appdata/bidrecordlist/`, {
-    headers: {
-      'Authorization': token ? `Token ${token}` : '',
-      'Content-Type': 'application/json'
-    }
-  }).then((res) => { callback(res, 'sucess') })
-    .catch(err => callback(err, err.response))
-}
-
-export const getBidlogbyagentdata = (callback: (arg0: any, arg1: string) => void, token: any,agentid:any) => {
-  instance.get(`/appdata/bidrecordlist/?user_id=`+agentid, {
-    headers: {
-      'Authorization': token ? `Token ${token}` : '',
-      'Content-Type': 'application/json'
-    }
-  }).then((res) => { callback(res, 'sucess') })
-    .catch(err => callback(err, err.response))
-}
-
-
-export const getBidlogbyagentnumberdata = (callback: (arg0: any, arg1: string) => void, token: any,agentid:any,num:any,pat:any) => {
-  instance.get(`/appdata/bidrecordlist/?user_id=`+agentid+"&number="+num+"&combination="+pat, {
-    headers: {
-      'Authorization': token ? `Token ${token}` : '',
-      'Content-Type': 'application/json'
-    }
-  }).then((res) => { callback(res, 'sucess') })
-    .catch(err => callback(err, err.response))
-}
-
-export const getuserList = (callback: (arg0: any, arg1: string) => void, token: any) => {
-  instance.get(`/user/profile_list/`, {
-    headers: {
-      'Authorization': token ? `Token ${token}` : '',
-      'Content-Type': 'application/json'
-    }
-  }).then((res) => { callback(res, 'sucess') })
-    .catch(err => callback(err, err.response))
-}
-
-export const getBidcountdata = (callback: (arg0: any, arg1: string) => void, token: any, data: any) => {
-  instance.post(`/appdata/bidrecord/count/`, data, {
-    headers: {
-      'Authorization': token ? `Token ${token}` : '',
-      'Content-Type': 'application/json'
-    }
-  }).then((res) => { callback(res, 'sucess') })
-    .catch(err => callback(err, err.response))
-}
-
-export const getBidcountagentdata = (callback: (arg0: any, arg1: string) => void, token: any,data:any) => {
-  instance.post(`/appdata/bidrecord/count/agent/`, data ,{
-    headers: {
-      'Authorization': token ? `Token ${token}` : '',
-      'Content-Type': 'application/json'
-    }
-  }).then((res) => { callback(res, 'sucess') })
-    .catch(err => callback(err, err.response))
-}
-
-
-export const postResultAppdata = (callback: (arg0: any, arg1: string) => void, token: any,data:any) => {
-  instance.post(`/appdata/gamedatalist/`, data ,{
-    headers: {
-      'Authorization': token ? `Token ${token}` : '',
-      'Content-Type': 'application/json'
+      'Authorization': 'Token'
     }
   }).then((res) => { callback(res, 'sucess') })
     .catch(err => callback(err, err.response))
--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const instance = axios.create({
-  baseURL:  "https://apifs.mediacannibal.com"
+  // baseURL:  "https://apifs.mediacannibal.com"
 })
 
 export default instance
