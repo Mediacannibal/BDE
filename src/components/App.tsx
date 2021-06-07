752cf4c1bf87620a9592b1ffeafc0ab533c37b53
HEMANTH KAVI <hemanthkavideveloper@gmail.com>
2021-06-07 13:28:25 +0530

    changes

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -28,8 +28,7 @@ import Notifications from './Notifications';
 import Report from "../components/AnalyticsReport/index";
 import TaskTimeLog from './TaskTimeLog';
 import AppGantt from './ChatProcess/AppGantt';
-import { getToken, onMessageListener} from '../../firebase'; 
-import Phoneauth from './Phoneauth';
+import { getToken, onMessageListener } from '../../firebase';
 
 const dashboard_screen = [
   { path: '/Home', component: HomeScreen },
@@ -49,32 +48,29 @@ const dashboard_screen = [
   { path: '/Notifications', component: Notifications },
   { path: '/report', component: Report },
   { path: '/AppGantt', component: AppGantt },
-  { path: '/Phoneauth', component: Phoneauth },
 ]
 
 const fullpage_screen = [
-  { path: '/', component: LoginScreen }, 
+  { path: '/', component: LoginScreen },
   { path: '/Meeting', component: MeetingScreen },
 ]
 
 const App = () => {
 
   useEffect(() => {
-   if(navigator.userAgent.toLowerCase().indexOf('safari/') > -1)
-    {
+    if (navigator.userAgent.toLowerCase().indexOf('safari/') > -1) {
       getToken()
     }
     ReactGA.pageview(window.location.pathname + window.location.search);
   }, []);
 
-if(navigator.userAgent.toLowerCase().indexOf('safari/') > -1)
-{  
-  onMessageListener().then((message:any) => {
-    console.log(message)
-  }).catch(err => console.log('failed: ', err));
-  
-  
-}
+  if (navigator.userAgent.toLowerCase().indexOf('safari/') > -1) {
+    onMessageListener().then((message: any) => {
+      console.log(message)
+    }).catch(err => console.log('failed: ', err));
+
+
+  }
 
   return (
 
