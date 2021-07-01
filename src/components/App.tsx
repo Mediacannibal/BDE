9a0938634d89bc1da313becb8395d26efd4d6652
Naveen <data@mediacannibal.com>
2021-07-01 15:23:02 +0530

    achgsdf

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,8 +1,9 @@
 import 'core-js/stable'
-import React, {  useState } from 'react'
+import React, {  useState,useEffect } from 'react'
 import 'regenerator-runtime/runtime'
 import { HashRouter as Router, Switch, Route, withRouter } from 'react-router-dom'
  
+import ReactGA from 'react-ga';
 
 import "./app.css";
 
@@ -20,12 +21,14 @@ import Dashboard from './Dashboard';
 import UserProfile from './UserMenuItems/UserProfile';
 import UserSettings from './UserMenuItems/UserSettings';
 import ChatProject from './ChatProject';
+import NewUserForm from './Forms/UserSetup';
 import TestSelectionForm from './Forms/TestSelection/TestSelectionForm';
 import ApiRecords from './Api Records';
 import Notifications from './Notifications';
 import Report from "../components/AnalyticsReport/index";
 import TaskTimeLog from './TaskTimeLog';
-import AppGantt from './ChatProcess/AppGantt'; 
+import AppGantt from './ChatProcess/AppGantt';
+import { getToken, onMessageListener} from '../../firebase'; 
 import MeetingRoom from './MeetingScreen/MeetingRoom';
 
 const dashboard_screen = [
@@ -40,6 +43,7 @@ const dashboard_screen = [
   { path: '/UserProfile', component: UserProfile },
   { path: '/UserSettings', component: UserSettings },
   { path: '/ChatProject', component: ChatProject },
+  { path: '/NewUserForm', component: NewUserForm },
   { path: '/TestSelectionForm', component: TestSelectionForm },
   { path: '/ApiRecords', component: ApiRecords },
   { path: '/Notifications', component: Notifications },
@@ -51,34 +55,48 @@ const dashboard_screen = [
 const fullpage_screen = [
   { path: '/', component: LoginScreen }, 
   { path: "/MeetingRoom/:roomID", component: MeetingRoom },
+   
 ]
 
 const App = () => {
- 
+
+  useEffect(() => {
+   if(navigator.userAgent.toLowerCase().indexOf('safari/') > -1)
+    {
+      getToken()
+    }
+    ReactGA.pageview(window.location.pathname + window.location.search);
+  }, []);
+
+if(navigator.userAgent.toLowerCase().indexOf('safari/') > -1)
+{  
+  onMessageListener().then((message:any) => {
+    console.log(message)
+  }).catch(err => console.log('failed: ', err));
+   
+}
 
   return (
-    <> 
 
-      <Router>
-        <Switch>
-          {fullpage_screen.map((Data: any) =>
+    <Router>
+      <Switch>
+        {fullpage_screen.map((Data: any) =>
+          <Route exact path={Data.path}>
+            <Data.component />
+          </Route>
+        )}
+
+        {dashboard_screen.map((Data: any) => {
+          const [blabla, setblabla] = useState()
+          return (
             <Route exact path={Data.path}>
-              <Data.component />
+              <Dashboard screen={<Data.component setheader_options={setblabla} />} screen_name={Data.path} header_options={blabla} />
             </Route>
-          )}
-
-          {dashboard_screen.map((Data: any) => {
-            const [blabla, setblabla] = useState()
-            return (
-              <Route exact path={Data.path}>
-                <Dashboard screen={<Data.component setheader_options={setblabla} />} screen_name={Data.path} header_options={blabla} />
-              </Route>
-            )
-          }
-          )}
-        </Switch>
-      </Router>
-    </>
+          )
+        }
+        )}
+      </Switch>
+    </Router>
   )
 }
 
