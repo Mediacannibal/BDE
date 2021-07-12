f76550e8cea14438cc6b70f05224a991c9eb371e
Naveen <data@mediacannibal.com>
2021-07-12 23:33:39 +0530

    sdfasdf

--- firebase.js
+++ firebase.js
@@ -1,7 +1,15 @@
-import firebase from 'firebase/app';
-import 'firebase/messaging';
-import "firebase/auth";
+// Firebase App (the core Firebase SDK) is always required and must be listed first
+import firebase from "firebase/app";
+// If you are using v7 or any earlier version of the JS SDK, you should import firebase using namespace import
+// import * as firebase from "firebase/app"
 
+// If you enabled Analytics in your project, add the Firebase SDK for Analytics
+import "firebase/analytics";
+
+// Add the Firebase products that you want to use
+import "firebase/auth";
+import "firebase/firestore";
+import 'firebase/messaging';
 
 var firebaseConfig = {
   apiKey: "AIzaSyBROSFdFQWbp8K2xMMjKaqazC4HP4grI5A",
@@ -20,55 +28,35 @@ const messaging = firebase.messaging();
 messaging.getToken({ vapidKey: "BHRovRsRVsGuEcsg3HaJ1fI4e_aBl9QYbkBSS7OHp7HzryXZm2nXQ2RvZrns1MNxZdVWGKACKMwAEGANZKqUR_M" });
 
 
-// const messaging = firebase.messaging.isSupported() ? firebase.messaging() : null
-
-// export const getToken = () => {
-
-
-//   return new Promise((resolve, reject) => {
-//     messaging?.requestPermission().then(() => {
-//       //  console.log("permission granted");
-//       return messaging?.getToken()
-//     }
-//     ).then((firebaseToken) => {
-//       resolve(firebaseToken);
-//       console.log(">>>>>", firebaseToken);
-//     })
-//       .catch((err) => {
-//         console.log(err);
-//         reject(err);
-//       });
-//   });
-// }
-
-
 export const getToken = (setTokenFound) => {
-  return messaging.getToken({ vapidKey: 'BHRovRsRVsGuEcsg3HaJ1fI4e_aBl9QYbkBSS7OHp7HzryXZm2nXQ2RvZrns1MNxZdVWGKACKMwAEGANZKqUR_M' }).then((currentToken) => {
+  const a = messaging.getToken({ vapidKey: 'BHRovRsRVsGuEcsg3HaJ1fI4e_aBl9QYbkBSS7OHp7HzryXZm2nXQ2RvZrns1MNxZdVWGKACKMwAEGANZKqUR_M' }).then((currentToken) => {
     if (currentToken) {
       console.log('current token for client: ', currentToken);
-      setTokenFound(true);
-      // Track the token -> client mapping, by sending to backend server
-      // show on the UI that permission is secured
+
     } else {
       console.log('No registration token available. Request permission to generate one.');
-      setTokenFound(false);
-      // shows on the UI that permission is required 
     }
   }).catch((err) => {
     console.log('An error occurred while retrieving token. ', err);
     // catch error while creating client token
-  });
+  })
+  const b = () => {
+    console.log("does not support");
+  }
+  return navigator.userAgent.toLowerCase().indexOf('safari/') > -1 ? a : b;
 }
 
-export const onMessageListener = () =>
-  new Promise((resolve) => {
-    firebase.messaging().onMessage(notification => {
-      alert('Notification received!', notification);
+export const onMessageListener = () => {
+  const a = new Promise((resolve) => {
+    messaging.onMessage((payload) => {
+      console.log("=====>", payload);
+      resolve(payload);
     });
   });
-
-messaging.onMessage(function (payload) {
-  console.log("Message received. ", payload);
-});
+  const b = () => {
+    console.log("does not support");
+  }
+  return navigator.userAgent.toLowerCase().indexOf('safari/') > -1 ? a : b;
+}
 
 export default firebase;
